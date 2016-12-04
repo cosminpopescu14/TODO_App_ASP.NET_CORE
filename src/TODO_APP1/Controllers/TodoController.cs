75614ad6547c5c23513fd543af1c3817a9166317
Cosmin Popescu <cosmin.popescu93@gmail.com>
2016-12-04 16:05:49 +0200

    4.12.2016

--- src/TODO_APP1/Controllers/TodoController.cs
+++ src/TODO_APP1/Controllers/TodoController.cs
@@ -5,14 +5,76 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Authorization;
 
+using TODO_APP1.DTO;
+using TODO_APP1.Models;
+
 namespace TODO_APP1.Controllers
 {
     public class TodoController : Controller
     {
+        private TODO_AppContext todoContext = new TODO_AppContext();
+
         [Authorize]
         public IActionResult Index()
         {
             return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [Authorize]
+        public ActionResult AddTodo(DTOTodo _todo)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    var todo = new Todos//create object todo
+                    {
+                        Title = _todo.Title,
+                        Description = _todo.Description,
+                        StartDate = _todo.Start,
+                        EndDate = _todo.End
+                    };
+
+                    Users user = new Users();
+                    todoContext.Add(todo);//insert in database
+                    todoContext.SaveChanges();                   
+                }
+                catch (Exception ex)
+                {
+
+                    throw ex;
+                }
+            }
+            return RedirectToAction("Index");
+        }
+
+        [Authorize]
+        [HttpGet]
+        public ActionResult GetTodos()
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    var todos = from todo in todoContext.Todos//select a todo from db using linq
+                                select new
+                                {
+                                    todo.Title,
+                                    todo.Description,
+                                    todo.StartDate,
+                                    todo.EndDate
+                                };
+                    return Json(todos);//return the todo in json format
+                }
+                catch (Exception ex)
+                {
+
+                    throw ex;
+                }
+            }
+            return RedirectToAction("Index");
         }
     }
 }
---
+++ src/TODO_APP1/DTO/DTOTodo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TODO_APP1.DTO
+{
+    public class DTOTodo
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime Start { get; set; }
+        public DateTime End { get; set; }
+    }
+}
